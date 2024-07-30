dc126de231d9904fe06c77d2f14ead1d35fc1013
stanislavets <stanislavets2009@mail.ru>
2024-07-30 12:07:32 +0300

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,14 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_word = root_word.lower()
+    for word in other_words:
+        word2 = word.lower()
+        if root_word in word or word2 in root_word:
+            same_words.append(word)
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
